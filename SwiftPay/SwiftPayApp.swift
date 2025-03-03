b075456ba2b57a0fcd7f4f43062f178123920e4d
Hadid Hardiansyah Saputra <hadidhardiansyah@gmail.com>
2025-03-03 21:21:34 +0700

    Initial Commit

---
+++ SwiftPay/SwiftPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPayApp.swift
+//  SwiftPay
+//
+//  Created by Hadid Hardiansyah Saputra on 03/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
